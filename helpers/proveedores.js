abfe8d1c1f6c859bb98ba5b25e9cddd5b29919cf
eyfile16 <eyfile16@gmail.com>
2025-02-13 11:57:54 -0500

    Delete helpers directory

--- helpers/proveedores.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const ProveedorSchema = new Schema({
-    nombre: { type: String, required: true },
-    contacto: { type: String, required: true },
-    telefono: { type: String, required: true }
-});
